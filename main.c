d6318fef636b8673333d51d7170b2cd606fb3dee
MaryKwawu <may419316@gmail.com>
2023-02-23 10:31:16 -0800

    made changes to main.c

--- main.c
+++ main.c
@@ -1,86 +1,74 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * main - shell interface
- * Description - This function mimics a shell and while running an
- * infinite loop
- * waits for user inputs and executes the command given by the user.
- * Return: 0 always
+ * free_data - frees data structure
+ *
+ * @datash: data structure
+ * Return: no return
  */
-
-int main(void)
+void free_data(data_shell *datash)
 {
-	char *cmd_array = NULL, *cmd = NULL, *cmd_path = NULL;
-	char **args = NULL;
-	size_t cmd_size = 0;
-	ssize_t n_read = 0;
+	unsigned int i;
 
-	while (1)
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		printf("#cisfun$ ");/*command prompt*/
-		n_read = getline(&cmd_array, &cmd_size, stdin);
-		cmd_array[n_read - 1] = 0;/* pop newline char */
-		if (is_exit(cmd_array))
-			break;
-		cmd = get_cmd(cmd_array);
-		cmd_path = locate_cmd(cmd);
-		if (cmd_path)
-		{
-			args = getcmd_array(cmd_path, cmd_array, cmd);
-			if (args)
-			{/*create child process*/
-				if (fork() == 0)
-				{
-					execve(*args, args, environ);
-					return (0);
-				}
-				else
-				{
-					wait(NULL);
-					free(cmd_path);
-					free_array(args);
-				}
-			}
-			else
-				free(cmd_path);
-		}
-		else
-			dprintf(STDERR_FILENO, "-%s: Command not found\n", cmd);
-		free(cmd);
+		free(datash->_environ[i]);
 	}
-	free(cmd_array);
-	return (0);
-}
 
+	free(datash->_environ);
+	free(datash->pid);
+}
 
 /**
- * getcmd_array - returns and array of commands passed NULL terminated
- * @cmd_path: path to the cmd executable
- * @cmd_args: array of commands passed to the commands line delimited by spaces
- * @cmd: the first line of the arguments
- * Return: array of all arguments passed to the shell NULL terminated
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
  */
-char **getcmd_array(const char *cmd_path, const char *cmd_args,
-		    const char *cmd)
+void set_data(data_shell *datash, char **av)
 {
-	size_t i = 0, words = 0, tmp_i = 0;
-	char **array = NULL;
+	unsigned int i;
 
-	while (cmd[i] && cmd[i] == cmd_args[i])
-		i++;
-	tmp_i = i;/* save the index of the next argument start or end*/
-	words++;
-	if (cmd_args[i] && cmd_args[i] == ' ')
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
 	{
-		i++;
-		tmp_i = i;/*save the start of the second argument*/
-		for (; cmd_args[i]; i++)
-			if (cmd_args[i] == ' ' && cmd_args[i + 1] != ' ')
-				words++;
-		words++;
-	} /* makes words to equal at least 2 */
-	words++;
-	array = create_array(words, cmd_path, cmd_args + tmp_i);
+		datash->_environ[i] = _strdup(environ[i]);
+	}
 
-	return (array);
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
 }
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
+}
+
